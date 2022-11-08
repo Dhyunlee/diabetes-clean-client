a3588890460fd7f05573f50cf3db227095087797
Dhyunlee <skyblue5030@naver.com>
2022-11-08 18:17:09 +0900

    Chore: axios, react-router-dom 설치

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -1,10 +1,6 @@
-import { Global } from "@emotion/react";
-import { reset } from "../../styles/reset";
-
 const App = () => {
   return (
     <div>
-      <Global styles={reset} />
       <div>
         리액트 프로젝트!
       </div>
