56090874c116d9737941beff818de07c6dfceb17
Dhyunlee <skyblue5030@naver.com>
2022-11-08 17:38:24 +0900

    xcxxc

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import AppLayout from './layouts/AppLayout';
-=======
-import AppLayout from 'layouts/AppLayout';
 import { Global } from "@emotion/react";
 import { reset } from 'styles/reset';
->>>>>>> 27b344e52cf974a626e076a56a40b4880d035c79
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
@@ -14,10 +10,6 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-=======
-    <Global styles={reset}/>
->>>>>>> 27b344e52cf974a626e076a56a40b4880d035c79
     <AppLayout />
   </React.StrictMode>
 );
--- src/layouts/AppLayout/index.jsx
+++ src/layouts/AppLayout/index.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Global } from "@emotion/react";
 import { reset } from "../../styles/reset";
 
@@ -6,11 +5,6 @@ const App = () => {
   return (
     <div>
       <Global styles={reset} />
-=======
-const App = () => {
-  return (
-    <div>
->>>>>>> 27b344e52cf974a626e076a56a40b4880d035c79
       <div>
         리액트 프로젝트!
       </div>
