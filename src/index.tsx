27b344e52cf974a626e076a56a40b4880d035c79
Dhyunlee <skyblue5030@naver.com>
2022-11-08 17:28:22 +0900

    Update: 폴더 경로 변경 및 절대 경로 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppLayout from './layouts/AppLayout';
+import AppLayout from 'layouts/AppLayout';
+import { Global } from "@emotion/react";
+import { reset } from 'styles/reset';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
@@ -8,6 +10,7 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
+    <Global styles={reset}/>
     <AppLayout />
   </React.StrictMode>
 );
