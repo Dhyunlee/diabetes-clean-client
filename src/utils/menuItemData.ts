ece19f93cf1a9765d351bca5a20f0a161fc668cc
Dhyunlee <skyblue5030@naver.com>
2022-11-20 08:54:02 +0900

    Refactor: Topbar 코드 정리

---
+++ src/utils/menuItemData.ts
@@ -0,0 +1,18 @@
+export const menuItemData = [
+    {
+      title: '기록',
+      path: '/memo',
+      cName: 'nav-text'
+    },
+    {
+      title: '스토리',
+      path: '/story',
+      cName: 'nav-text'
+    },
+    {
+      title: '마이페이지',
+      path: '/mypage',
+      cName: 'nav-text'
+    },
+  ];
+  
