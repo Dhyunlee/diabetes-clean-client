110899ffae2ba4c44f90bb349d412ff370809327
Dhyunlee <skyblue5030@naver.com>
2022-11-23 02:24:02 +0900

    Setting: cors, proxy 설정

--- src/pages/Memo/index.tsx
+++ src/pages/Memo/index.tsx
@@ -1,4 +1,10 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 const Memo = () => {
+  useEffect(() => {
+    const user = axios.get('/api/v1/users').then(res => console.log(res))
+  }, [])
   return (
     <div>memo</div>
   )
