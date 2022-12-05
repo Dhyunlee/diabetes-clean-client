455a359347175d39ebc08d4e80e152378296a5d0
Dhyunlee <skyblue5030@naver.com>
2022-12-05 13:19:57 +0900

    Feat: WriteMemo 컴포넌트 생성

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import MyPage from "pages/Mypage";
 import NotFound from "pages/NotFound";
 import SignUp from "pages/SignUp";
 import Story from "pages/Story";
+import WriteMemo from "pages/WriteMemo";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 const RouterContainer = () => {
@@ -13,6 +14,7 @@ const RouterContainer = () => {
       <Route path="signup" element={<SignUp />} />
       <Route path="login" element={<Login />} />
       <Route path="memo/*" element={<Memo />} />
+      <Route path="newmemo" element={<WriteMemo/>} />
       <Route path="story" element={<Story />} />
       <Route path="mypage" element={<MyPage />} />
       <Route path="*" element={<NotFound />} />
