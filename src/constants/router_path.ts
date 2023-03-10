0df755833dce80a016addc27fb2cc5ec2a5e7a4c
Dhyunlee <skyblue5030@naver.com>
2023-03-10 17:21:47 +0900

    Feat(constants): API_PATH, ROUTER_PATH 추가

---
+++ src/constants/router_path.ts
@@ -0,0 +1,10 @@
+export const ROUTER_PATH = {
+  INDEX: '/',  
+  LOGIN: "/login",
+  SIGNUP: 'signup',
+  SAVE_MEMO: '/memo/save',
+  MEMO_DIABETES: '/memo/diabetes',
+  MEMO: '/memo/*',
+  MYPAGE: '/mypage', 
+  STORY: '/story',
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { ROUTER_PATH } from "constants/router_path";
 import Login from "pages/Login";
 import Memo from "pages/Memo";
 import MyPage from "pages/Mypage";
@@ -9,16 +10,18 @@ import WriteMemo from "pages/WriteMemo";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 const RouterContainer = () => {
+  const { INDEX, LOGIN, SIGNUP, MEMO, MEMO_DIABETES, SAVE_MEMO, MYPAGE, STORY } = ROUTER_PATH;
+
   return (
     <Routes>
-      <Route path="/" index element={<Navigate replace to="/memo/diabetes" />} />
-      <Route path="signup" element={<SignUp />} />
-      <Route path="login" element={<Login />} />
-      <Route path="memo/*" element={<Memo />} />
-      <Route path="newmemo" element={<WriteMemo/>} />
-      <Route path="story" element={<Story />} />
-      <Route path="mypage" element={<MyPage />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path={INDEX} index element={<Navigate replace to={MEMO_DIABETES} />} />
+      <Route path={SIGNUP} element={<SignUp />} />
+      <Route path={LOGIN} element={<Login />} />
+      <Route path={MEMO} element={<Memo />} />
+      <Route path={SAVE_MEMO} element={<WriteMemo />} />
+      <Route path={STORY} element={<Story />} />
+      <Route path={MYPAGE} element={<MyPage />} />
+      <Route path={"*"} element={<NotFound />} />
     </Routes>
   );
 };
