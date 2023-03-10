ba1e2137ae7b3ef6a1011095fe8aa26741b51237
Dhyunlee <skyblue5030@naver.com>
2023-03-10 11:10:16 +0900

    chore: axios 재설정

--- src/utils/apis/diabetesApis.ts
+++ src/utils/apis/diabetesApis.ts
@@ -1,18 +1,15 @@
-import axios from "axios";
 import { QueryFunctionContext } from "react-query";
+import api from "utils/axios";
 
 const getDiabetes = async (userId: QueryFunctionContext<(string | undefined)[], any>) => {
-    try {
-      const { data } = await axios.get(`/api/v1/diabetes/users/${userId}`, {
-        withCredentials: true,
-      });
-      return data;
-    } catch (error: any) {
-      throw error.response;
-    }
-  };
-
-
-  export {
-    getDiabetes
+  try {
+    const { data } = await api.get(`/api/v1/diabetes/users/${userId}`, {
+      withCredentials: true,
+    });
+    return data;
+  } catch (error: any) {
+    throw error.response;
   }
+};
+
+export { getDiabetes };
