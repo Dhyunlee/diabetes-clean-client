70e9705e1eb6a1cb249ad36ef593aabd43412af9
Dhyunlee <skyblue5030@naver.com>
2023-03-30 07:07:14 +0900

    Feat(Base/*): Input, SubBtnMenu, ContentsInfo 구현

---
+++ src/components/Base/Input/styles.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const InputInterface = styled.input`
+  width: 100%;
+  height: 100%;
+  padding: 5px;
+  outline: none;
+  font-size: 16px;
+  border-radius: 8px;
+  border: solid 1px #c4c4c4;
+`;
