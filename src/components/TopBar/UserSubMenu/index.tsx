23a2a98c8bc4255f5f2cd6f9916854a98ac19040
Dhyunlee <skyblue5030@naver.com>
2023-04-14 17:14:55 +0900

    Chore: TopBar folder 추가, 폴더명 따로 변경할 방법이 없어서 폴더 삭제후 다시 업로드함

---
+++ src/components/TopBar/UserSubMenu/index.tsx
@@ -0,0 +1,47 @@
+import React, { useCallback } from "react";
+
+import Menu from "components/Base/Menu";
+import { useQueryClient } from "react-query";
+import { Link, useNavigate } from "react-router-dom";
+import { Li, MenuContainer } from "./styles";
+import api from "utils/axios";
+import useStorage from "utils/functions/useStorage";
+
+interface Props {
+  showUserSubMenu: boolean;
+  handleCloseMenu: () => void;
+}
+const UserSubMenu = ({ showUserSubMenu, handleCloseMenu }: Props) => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const {removeStorage} = useStorage;
+
+  const handleLogOut = useCallback(() => {
+    api.get("/api/v1/auth/logout", { withCredentials: true }).then((res) => {
+      removeStorage('accessToken')
+      queryClient.setQueryData("user", false);
+      navigate("/login", { replace: true });
+    });
+
+    handleCloseMenu();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Menu showMenu={showUserSubMenu} onCloseModal={handleCloseMenu}>
+      <MenuContainer className="user-sub-menu">
+        <Li onClick={() => navigate("/mypage")}>
+          <Link onClick={handleCloseMenu} to={"/mypage"}>
+            마이페이지
+          </Link>
+        </Li>
+        <Li onClick={handleLogOut}>
+          <button onClick={handleLogOut}>로그아웃</button>
+        </Li>
+      </MenuContainer>
+    </Menu>
+  );
+};
+
+export default React.memo(UserSubMenu);
---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Sidebar from "components/Base/Sidebar";
+import { Navbar, OverWrap } from "./styles";
+import UserMenu from "./UserMenu";
+
+function Topbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const showSidebar = () => setIsOpen(true);
+  const showCloseSidebar = () => setIsOpen(false);
+
+  return (
+    <>
+      <Navbar className="navbar">
+        <div className="menu-left">
+          <div>
+            <button className="menu-bars">
+              <span onClick={showSidebar}><FaIcons.FaBars /></span>
+            </button>
+          </div>
+          <div className="page-title">
+            <Link to="/">당클린</Link>
+          </div>
+        </div>
+        <div className="menu-right">
+          <UserMenu/>
+        </div>
+      </Navbar>
+      {isOpen && <OverWrap onClick={showCloseSidebar}></OverWrap>}
+      <Sidebar isOpen={isOpen} showCloseSidebar={showCloseSidebar} />
+    </>
+  );
+}
+
+export default Topbar;
