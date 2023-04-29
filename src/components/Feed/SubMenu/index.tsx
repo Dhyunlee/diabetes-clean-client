e28ee52cc83cf4f8a94c0a42f9db5e5abce10b9b
Dhyunlee <skyblue5030@naver.com>
2023-04-30 06:15:56 +0900

    Feat: 드롭다운 메뉴 구현

---
+++ src/components/Feed/SubMenu/index.tsx
@@ -0,0 +1,59 @@
+import {
+  MouseEvent,
+  useCallback,
+  useEffect,
+  useRef,
+} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { SubMenuContainer, SubMenuWrap } from "./styles";
+
+interface Props {
+  showSubMenu: boolean;
+  setShowSubMenu: React.Dispatch<React.SetStateAction<boolean>>;
+  onShowSubMenu?: () => void;
+  onCloseMenu: () => void;
+}
+
+const SubMenu = ({ showSubMenu, setShowSubMenu, onCloseMenu }: Props) => {
+  const navigate = useNavigate();
+  const MenuRef = useRef<HTMLDivElement | null>(null);
+
+  const stopPropagation = useCallback((e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+  }, []);
+
+  useEffect(() => {
+    const onClickOutSide = (e: any) => {
+      if (showSubMenu && !MenuRef.current?.contains(e.target)) {
+        setShowSubMenu(false);
+      }
+    };
+    document.addEventListener("mousedown", onClickOutSide);
+    return () => {
+      document.removeEventListener("mousedown", onClickOutSide);
+    };
+  }, [setShowSubMenu, showSubMenu]);
+
+  console.log(showSubMenu);
+  if (!showSubMenu) return null;
+
+  return (
+    <SubMenuWrap ref={MenuRef}>
+      <SubMenuContainer onMouseDown={stopPropagation}>
+        <div className="menu-list" onClick={() => navigate("/mypage")}>
+          <Link onClick={onCloseMenu} to={"/mypage"}>
+            게시글 수정
+          </Link>
+        </div>
+        <div
+          className="menu-list"
+          onClick={() => console.log("삭제 기능 중비중...")}
+        >
+          <button onClick={onCloseMenu}> 게시글 삭제</button>
+        </div>
+      </SubMenuContainer>
+    </SubMenuWrap>
+  );
+};
+
+export default SubMenu;
