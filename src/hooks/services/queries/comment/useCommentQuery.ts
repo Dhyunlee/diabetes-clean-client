e96f724084acc5100812f7f2d3cf7e599fc44bd6
Dhyunlee <skyblue5030@naver.com>
2023-05-22 00:45:56 +0900

    Feat: comment react-query 구현

--- src/hooks/services/queries/comment/useCommentQuery.ts
+++ src/hooks/services/queries/comment/useCommentQuery.ts
@@ -1,13 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { COMMENT_KEY } from "constants/query_key";
-import { IContentsResponse } from "models/db";
 import { getAllComment } from "utils/apis/comment";
 
-const useCommentQuery = () => {
-  return useQuery<IContentsResponse>({
-    queryKey: [COMMENT_KEY],
-    queryFn: () => getAllComment(),
-  });
+const useCommentQuery = (contentsId: string) => {
+   return useQuery({
+      queryKey: [COMMENT_KEY, contentsId],
+      queryFn: () => getAllComment(contentsId),
+      enabled: !!contentsId,
+    });
 };
 
 export default useCommentQuery;
