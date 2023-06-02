eca264f05072cc57bf58e313de312ecef281f2ed
Dhyunlee <skyblue5030@naver.com>
2023-06-03 01:17:39 +0900

    Chore: 댓글 react-query query 타입 수정

--- src/hooks/services/queries/comment/useCommentQuery.ts
+++ src/hooks/services/queries/comment/useCommentQuery.ts
@@ -1,9 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import { COMMENT_KEY } from "constants/query_key";
+import { ICommentResponse } from "models/db";
 import { getAllComment } from "utils/apis/comment";
 
 const useCommentQuery = (contentsId: string) => {
-   return useQuery({
+   return useQuery<ICommentResponse | undefined, AxiosError>({
       queryKey: [COMMENT_KEY, contentsId],
       queryFn: () => getAllComment(contentsId),
       enabled: !!contentsId,
