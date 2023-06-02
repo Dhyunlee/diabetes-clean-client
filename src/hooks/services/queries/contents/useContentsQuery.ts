0f75b0bc5947dc35fab75d83ffb74950aa4ce1b5
Dhyunlee <skyblue5030@naver.com>
2023-06-03 01:17:54 +0900

    Chore: Contents react-query query 타입 수정

--- src/hooks/services/queries/contents/useContentsQuery.ts
+++ src/hooks/services/queries/contents/useContentsQuery.ts
@@ -1,9 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import { CONTENTS_KEY } from "constants/query_key";
+import { IContentsResponse } from "models/db";
 import { getAllContents } from "utils/apis/contents";
 
 const useContentsQuery = () => {
-  return useQuery({
+  return useQuery<IContentsResponse | undefined, AxiosError>({
     queryKey: [CONTENTS_KEY],
     queryFn: () => getAllContents(),
   });
