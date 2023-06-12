94a99bacf57af42e417455f92a689a255c2ab226
Dhyunlee <skyblue5030@naver.com>
2023-06-12 12:49:14 +0900

    Feat: 유저 프로필 구현

---
+++ src/components/My/UserProfile/styles.ts
@@ -0,0 +1,56 @@
+import { palette } from "libs/palette";
+import styled from "@emotion/styled";
+
+export const ProfileBlock = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+  padding: 45px 0;
+  margin: 30px 0;
+  box-shadow: 0px 0px 12px -3px rgb(0 0 0 / 20%);
+  .user-info {
+    text-align: center;
+  }
+`;
+
+export const ProfileContainer = styled.div`
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  border-radius: 5px;
+  border: 1px solid lightgray;
+`;
+
+export const EditBtnWrap = styled.div`
+  display: flex;
+  gap: 5px;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid gray;
+  background-color: ${palette.gray[1]};
+  border-radius: 5px;
+  padding: 5px;
+  cursor: pointer;
+  user-select: none;
+  .icon-wrap {
+    width: 15px;
+    position: relative;
+    .icon {
+      position: absolute;
+      top: -9px;
+      font-size: 18px;
+    }
+  }
+  .btn-name {
+    font-size: 15px;
+  }
+
+  &:active {
+    transform: translateY(3px);
+  }
+`;
