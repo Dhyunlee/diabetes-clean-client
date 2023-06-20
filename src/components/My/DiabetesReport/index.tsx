d077059ebada6108b2a537c0d9715e855dad7979
Dhyunlee <skyblue5030@naver.com>
2023-06-21 02:44:20 +0900

    Chore: NavMenu import 적용

--- src/components/My/DiabetesReport/index.tsx
+++ src/components/My/DiabetesReport/index.tsx
@@ -1,6 +1,14 @@
-import DateSelect from "../DateSelect"
+import NavMenu from "components/Base/NavMenu"
 import ReportChart from "../ReportChart"
 import { Title } from "../styles"
+import { palette } from "libs/palette";
+
+const periods = [
+  { id: 1, text: "오늘" },
+  { id: 2, text: "한 주" },
+  { id: 3, text: "한 달" },
+  { id: 4, text: "3개월" },
+];
 
 const DiabetesReport = () => {
   return (
@@ -9,7 +17,7 @@ const DiabetesReport = () => {
         <span>당수치 통계</span>
       </Title>
       <div>
-      <DateSelect />
+       <NavMenu lists={periods} bgColor={palette.indigo[3]} fontSize={'18px'} /> 
       <ReportChart/>
       </div>
     </div>
