ccb9b21f0852f3b206a211f3848d736165cb3477
Dhyunlee <skyblue5030@naver.com>
2023-06-27 04:25:27 +0900

    Chore: 버튼 공통 컴포넌트 타입 수정

--- src/components/Base/Button/style.ts
+++ src/components/Base/Button/style.ts
@@ -1,9 +1,20 @@
 import styled from "@emotion/styled";
 import { palette } from "libs/palette";
 
-export const ButtonInterface = styled.button`
-  width: 80px;
-  height: 30px;
+export const ButtonInterface = styled.button<{
+  posX?: string | number;
+  posY?: string | number;
+  size?: string | number;
+}>`
+  width: ${(props) =>
+    (typeof props.posX === "number" ? props.posX + "px" : props.posX) ||
+    "86px"};
+  height: ${(props) =>
+    (typeof props.posY === "number" ? props.posY + "px" : props.posY) ||
+    "35px"};
+  font-size: ${(props) =>
+    (typeof props.size === "number" ? props.size + "px" : props.size) ||
+    "16px"};
   background-color: transparent;
   border: 1px solid #ced4da;
   border-radius: 8px;
