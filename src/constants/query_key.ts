8413c5d3c5243b742f0e50e0ca40d61e58cdb2f8
Dhyunlee <skyblue5030@naver.com>
2023-07-21 08:22:28 +0900

    Feat: follow axios 구현

--- src/constants/query_key.ts
+++ src/constants/query_key.ts
@@ -2,3 +2,4 @@ export const USER_KEY = "user";
 export const DIABETES_KEY = "diabetes";
 export const CONTENTS_KEY = "contents";
 export const COMMENT_KEY = "comment";
+export const FOLLOW_KEY = "follow";
---
+++ src/utils/apis/follow.ts
@@ -0,0 +1,46 @@
+import { API_PATH } from "constants/api_path";
+import api from "utils/axios";
+
+//patch api/v1/users/:id/follow
+const { USER_API } = API_PATH;
+
+const follow = async (userId: string) => {
+  if (!userId) return;
+  try {
+    const { data } = await api.patch(`${USER_API}/${userId}/follow`, {
+      withCredentials: true
+    });
+    return data;
+  } catch (error: any) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// patch api/v1/users/:id/unfollow
+const unFollow = async (userId: string | null) => {
+  if (!userId) return;
+  try {
+    const { data } = await api.patch(`${USER_API}/${userId}/unfollow`, {
+      withCredentials: true
+    });
+    return data;
+  } catch (error: any) {
+    console.error(error);
+    throw error;
+  }
+};
+const getFollow = async (userId: string | null) => {
+  if (!userId) return;
+  try {
+    const { data } = await api.get(`${USER_API}/${userId}/follow`, {
+      withCredentials: true
+    });
+    return data;
+  } catch (error: any) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export { follow, unFollow, getFollow };
