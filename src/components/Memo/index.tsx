8f2805ad6751e587947b332d4f33d1ada03713be
donghyunami <skyblue5030@naver.com>
2023-11-02 14:12:00 +0900

    feat: 당수치 날짜별로 묶어서 정렬

--- src/components/Memo/index.tsx
+++ src/components/Memo/index.tsx
@@ -1,53 +1,18 @@
-import { useState, useEffect, useMemo, useCallback } from "react";
-import { Route, Routes } from "react-router-dom";
-import { useRecoilValue } from "recoil";
+import { useState, useMemo, useCallback } from "react";
+// import { Route, Routes } from "react-router-dom";
 import dayjs from "dayjs";
 import DateArea from "components/Memo/Base/DateArea";
-import Submenu from "components/Memo/Base/Submenu";
 import Diabetes from "components/Memo/DiabetesList";
-import Diet from "components/Memo/Diet";
 import SideBtnMenu from "components/Base/SideBtnMenu";
 import alertHandler from "utils/functions/alertHandler";
-import { getDiabetes } from "utils/apis/diabetesApis";
-import { IDiabetesInfo, IDiabetesResponse } from "models/data";
-import { userState } from "store/userState";
-import { useAPIByIdQuery } from "hooks/service/queries";
 import { ROUTER_PATH } from "constants/router_path";
-import { DIABETES_KEY } from "constants/query_key";
-
 import { Container } from "styles/common";
 import { MemoContents, MemoHeader } from "./styles";
 
 const MemoList = () => {
-  const { SAVE_MEMO_DIABETES, SAVE_MEMO_DIET } = ROUTER_PATH;
-  const { _id: userId } = useRecoilValue(userState);
+  const { SAVE_MEMO_DIABETES } = ROUTER_PATH;
   const [curDate, setCurDate] = useState(dayjs());
   const [today] = useState(dayjs().format("YYYY-MM"));
-  const [processData, setProcessData] = useState<IDiabetesInfo[]>([]);
-
-  const {
-    data: diabetesData,
-    isError,
-    isLoading
-  } = useAPIByIdQuery<IDiabetesResponse>(userId, DIABETES_KEY, getDiabetes);
-
-  useEffect(() => {
-    const startOfDate = dayjs(curDate).startOf("month").format("YYYYMMDD");
-    const endOfDate = dayjs(curDate).endOf("month").format("YYYYMMDD");
-
-    const thisMonthData = (
-      diabetesData?.diabetesInfo as IDiabetesInfo[]
-    )?.filter((item: IDiabetesInfo) => {
-      const fomattedCreatedAt = dayjs(item.createdAt).format("YYYY-MM-DD");
-      const date = parseInt(fomattedCreatedAt.split("-").join(""), 10);
-      if (Number(startOfDate) <= date && date <= Number(endOfDate)) {
-        return item;
-      }
-    });
-    if (thisMonthData) {
-      setProcessData(thisMonthData);
-    }
-  }, [curDate, diabetesData?.diabetesInfo]);
 
   const currentDate = useMemo(
     () =>
@@ -56,6 +21,7 @@ const MemoList = () => {
       }월`,
     [curDate]
   );
+
   const incrementDate = useCallback(() => {
     const today_ = Number(today.split("-").join(""));
     const curDate_ = Number(curDate.format("YYYY-MM").split("-").join(""));
@@ -89,15 +55,12 @@ const MemoList = () => {
     [SAVE_MEMO_DIABETES]
   );
 
-  if (isLoading) return <div>당수치 내역을 불러오는중입니다.</div>;
-  if (isError) return <div>데이터를 가져오는 실패했어요</div>;
-
   return (
     <Container>
       <MemoHeader>
-        <div className="memo-title">
+        <h1 className="memo-title">
           <span>기록 내역</span>
-        </div>
+        </h1>
         {/* <Submenu /> */} {/* <-- 식단 기능 추가후 활성화 --> */}
         <DateArea
           currentDate={currentDate}
@@ -107,13 +70,15 @@ const MemoList = () => {
         <br />
       </MemoHeader>
       <MemoContents>
-        <Routes>
+        {/* 아래 주석은 식단 CRUD 개발하면 활성화하기 */}
+        {/* <Routes>
           <Route
             path="diabetes"
             element={<Diabetes diabetesInfo={processData} />}
           />
           <Route path="diet" element={<Diet />} />
-        </Routes>
+        </Routes> */}
+        <Diabetes curDate={curDate} />
       </MemoContents>
       <SideBtnMenu menuItem={menuItem} />
     </Container>
